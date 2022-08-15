fae372cbe76334219e1ccaac3e1f66fa4bb0255f
Abuzeid <abuzeid1999@gmail.com>
2022-08-15 13:13:16 +0200

    1.2.9

--- src/globalComponents/Picture/index.js
+++ src/globalComponents/Picture/index.js
@@ -20,39 +20,39 @@ function Picture({
         <source
           media="(max-width: 360px)"
           srcSet={_360?.src}
-          width={`${_360?.imageWidth}px`}
-          height={`${_360?.imageWidth / aspectRatio}px`}
+          // width={`${_360?.imageWidth}px`}
+          // height={`${_360?.imageWidth / aspectRatio}px`}
         />
       )}
       {_720 && (
         <source
           media="(max-width: 720px)"
           srcSet={_720?.src}
-          width={`${_720?.imageWidth}px`}
-          height={`${_720?.imageWidth / aspectRatio}px`}
+          // width={`${_720?.imageWidth}px`}
+          // height={`${_720?.imageWidth / aspectRatio}px`}
         />
       )}
       {_1080 && (
         <source
           media="(max-width: 1080px)"
           srcSet={_1080?.src}
-          width={`${_1080?.imageWidth}px`}
-          height={`${_1080?.imageWidth / aspectRatio}px`}
+          // width={`${_1080?.imageWidth}px`}
+          // height={`${_1080?.imageWidth / aspectRatio}px`}
         />
       )}
       {_1440 && (
         <source
           media="(max-width: 1440px)"
           srcSet={_1440?.src}
-          width={`${_1440?.imageWidth}px`}
-          height={`${_1440?.imageWidth / aspectRatio}px`}
+          // width={`${_1440?.imageWidth}px`}
+          // height={`${_1440?.imageWidth / aspectRatio}px`}
         />
       )}
 
       <img
         loading={loading ? loading : "lazy"}
-        width={`${_1920.imageWidth}px`}
-        height={`${_1920?.imageWidth / aspectRatio}px`}
+        // width={`${_1920.imageWidth}px`}
+        // height={`${_1920?.imageWidth / aspectRatio}px`}
         className={imageClassName ? imageClassName : ""}
         src={_1920.src}
         alt={_1920.alt}
